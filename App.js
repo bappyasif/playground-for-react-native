92bb0025a3b968d337dc351b5082c585b331bddb
bappyasif <bappyasif@hotmail.com>
2024-03-16 01:19:52 +0600

    starting to carrying out first rn project
    * made changes to make this project ready
    * working on making a cards rendering project

--- App.js
+++ App.js
@@ -1,53 +1,18 @@
-import { Alert, Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import CustomButton from "./components/CustomButton";
-// import CustomButtonIOS from "./components/CustomButton.ios";
-// import CustomButtonAndroid from "./components/CustomButton.android";
+import { Platform, SafeAreaView, StyleSheet, View } from "react-native";
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.text}>Welcome!</Text>
-          {/* <CustomButtonIOS onPress={() => Alert.alert("Ios button is pressed!!")} title={"Press Me"} />
-          <CustomButtonAndroid onPress={() => Alert.alert("Android button is pressed!!")} title={"Press Me"} /> */}
-
-          {/* based on os signature react native will render component accordingly */}
-
-          {/* for more is platform based rendering using Platform Extension is recommended where you will have defined os based components and during runtime react native will decide which components to render */}
-          <CustomButton onPress={() => Alert.alert("Ios button is pressed!!")} title={"Press Me"} />
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <PokemonCard />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: "plum",
-  },
   container: {
     flex: 1,
-    backgroundColor: "plum",
+    backgroundColor: "#f5f5f5",
     paddingTop: Platform.OS === "android" ? 26 : 0
   },
-  box: {
-    padding: 20
-  },
-  text: {
-    ...Platform.select({
-      ios: {
-        color: "purple",
-        fontSize: 29,
-        fontStyle: "italic"
-      },
-      android: {
-        color: "blue",
-        fontSize: 24
-      },
-    }),
-    fontWeight: "bold",
-    textAlign: "center"
-  }
 });
