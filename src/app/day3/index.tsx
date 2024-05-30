249e455c1376a74e83581dc151ec6333c742990b
bappyasif <bappyasif@hotmail.com>
2024-05-30 20:51:38 +0600

    with markdown
    * added an editor and a previewer
    * adjusted codes to reflect these changes

--- src/app/day3/index.tsx
+++ src/app/day3/index.tsx
@@ -1,12 +1,21 @@
 import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
 import { Link, Stack, usePathname } from 'expo-router'
+import MarkdownDisplay from '@/components/day3/markdown-display'
+
+const sampleMd = `
+# sample header
+
+integrating markdown with **React Native**
+`
 
 const DayDetailsScreen = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{flex: 1}}>
             <Stack.Screen options={{ title: `Day 3: Markdown project` }} />
             <Text style={{fontFamily: "Amatic", fontSize: 99}}>DayDetailsScreen</Text>
+
+            <MarkdownDisplay>{sampleMd}</MarkdownDisplay>
 
             <Link href={"/"}>Home Screen</Link>
 
