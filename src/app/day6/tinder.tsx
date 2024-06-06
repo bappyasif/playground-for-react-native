ab7219ba73a7f3bf360552646009e73ceda13d86
bappyasif <bappyasif@hotmail.com>
2024-06-07 02:24:50 +0600

    added functionalities
    * mkaing use of reanimated to make do cards have effect like in tinder swipes
    * kept older attempts for reference

--- src/app/day6/tinder.tsx
+++ src/app/day6/tinder.tsx
@@ -1,59 +1,100 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import TinderCard from '@/components/day6/tinder-card'
+import TinderCard from '@/components/day6/tinder-card';
 import { Stack } from 'expo-router';
-import { useSharedValue } from 'react-native-reanimated';
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import {
+  interpolate,
+  useAnimatedReaction,
+  useDerivedValue,
+  useSharedValue,
+  withDecay,
+  withSpring,
+  runOnJS,
+} from 'react-native-reanimated';
 
-const dummyUsers = [
-    {
-        id: 1,
-        image:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
-        name: 'Dani',
-    },
-    {
-        id: 2,
-        image:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
-        name: 'Jon',
-    },
-    {
-        id: 3,
-        image:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/3.jpg',
-        name: 'Dani',
-    },
-    {
-        id: 4,
-        image:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/4.jpeg',
-        name: 'Alice',
-    },
-    {
-        id: 5,
-        image:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/5.jpg',
-        name: 'Dani',
-    },
-    {
-        id: 6,
-        image:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/6.jpg',
-        name: 'Kelsey',
-    },
+const dummuUsers = [
+  {
+    id: 1,
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/1.jpg',
+    name: 'Dani',
+  },
+  {
+    id: 2,
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/2.jpg',
+    name: 'Jon',
+  },
+  {
+    id: 3,
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/3.jpg',
+    name: 'Dani',
+  },
+  {
+    id: 4,
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/4.jpeg',
+    name: 'Alice',
+  },
+  {
+    id: 5,
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/5.jpg',
+    name: 'Dani',
+  },
+  {
+    id: 6,
+    image:
+      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-images/6.jpg',
+    name: 'Kelsey',
+  },
 ];
 
-const TinderSwipeScreen = () => {
-    const activeIndex = useSharedValue(0)
+const TinderScreen = () => {
+  const [users, setUsers] = useState(dummuUsers);
+  const activeIndex = useSharedValue(0);
+  const [index, setIndex] = useState(0);
 
-    return (
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Stack.Screen options={{headerShown: false}} />
+  useAnimatedReaction(
+    () => activeIndex.value,
+    (value, prevValue) => {
+      if (Math.floor(value) !== index) {
+        runOnJS(setIndex)(Math.floor(value));
+      }
+    }
+  );
 
-            {dummyUsers.map((user, idx) => <TinderCard key={user.id} user={user} numOfCards={dummyUsers.length} currIndex={idx} activeIndex={activeIndex} />)}
-            {/* <TinderCard /> */}
-        </View>
-    )
-}
+  useEffect(() => {
+    if (index > users.length - 3) {
+      console.warn('Last 2 cards remining. Fetch more!');
+      setUsers((usrs) => [...usrs, ...dummuUsers.reverse()]);
+    }
+  }, [index]);
 
-export default TinderSwipeScreen
+  const onResponse = (res: boolean) => {
+    console.log('on Response: ', res);
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Stack.Screen options={{ headerShown: false }} />
+      {/* <Text style={{ top: 70, position: 'absolute' }}>
+        Current index: {index}
+      </Text> */}
+      {users.map((user, index) => (
+        <TinderCard
+          key={`${user.id}-${index}`}
+          user={user}
+          numOfCards={users.length}
+          index={index}
+          activeIndex={activeIndex}
+          onResponse={onResponse}
+        />
+      ))}
+    </View>
+  );
+};
+
+export default TinderScreen;
--- src/components/day6/tinder-card.tsx
+++ src/components/day6/tinder-card.tsx
@@ -1,161 +1,166 @@
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-import Animated, { SharedValue, interpolate, useAnimatedStyle } from 'react-native-reanimated';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 
-export const TinderCardWidth = Dimensions.get("screen").width * 0.8
+import Animated, {
+  SharedValue,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import { PanGesture } from 'react-native-gesture-handler';
 
-// export const TinderCardHeight = Dimensions.get("screen").height * 1.67
+const screenWidth = Dimensions.get('screen').width;
+export const tinderCardWidth = screenWidth * 0.8;
 
-type CardProps = {
-    user: {
-        name: string,
-        image: string
-    },
-    currIndex: number,
-    numOfCards: number,
-    activeIndex: SharedValue<number>
-}
+type TinderCard = {
+  user: {
+    image: string;
+    name: string;
+  };
+  numOfCards: number;
+  index: number;
+  activeIndex: SharedValue<number>;
+  onResponse: (a: boolean) => void;
+};
 
-export default function TinderCard({ user, currIndex, numOfCards, activeIndex }: CardProps) {
-    //  range
+const TinderCard = ({
+  user,
+  numOfCards,
+  index,
+  activeIndex,
+  onResponse,
+}: TinderCard) => {
+  const translationX = useSharedValue(0);
 
-    const animatedCard = useAnimatedStyle(() => ({
-        // opacity: .5
-        opacity: interpolate(
-            activeIndex.value,
-            [currIndex - 1, currIndex, currIndex + 1],
-            [1 - 1 / 5, 1, 1]
+  const animatedCard = useAnimatedStyle(() => ({
+    opacity: interpolate(
+      activeIndex.value,
+      [index - 1, index, index + 1],
+      [1 - 1 / 5, 1, 1]
+    ),
+    transform: [
+      {
+        scale: interpolate(
+          activeIndex.value,
+          [index - 1, index, index + 1],
+          [0.95, 1, 1]
         ),
+      },
+      {
+        translateY: interpolate(
+          activeIndex.value,
+          [index - 1, index, index + 1],
+          [-30, 0, 0]
+        ),
+      },
+      {
+        translateX: translationX.value,
+      },
+      {
+        rotateZ: `${interpolate(
+          translationX.value,
+          [-screenWidth / 2, 0, screenWidth / 2],
+          [-15, 0, 15]
+        )}deg`,
+      },
+    ],
+  }));
 
-        transform: [
-            {
-                scale: interpolate(
-                    activeIndex.value, 
-                    [currIndex - 1, currIndex, currIndex + 1],
-                    [0.95, 1, 1.1]
-                )
-            },
-            {
-                translateY: interpolate(
-                    activeIndex.value,
-                    [currIndex - 1, currIndex, currIndex + 1],
-                    [-27, 0, 0]
-                )
-            }
-        ]
-    }))
+  const gesture = Gesture.Pan()
+    .onChange((event) => {
+      translationX.value = event.translationX;
 
-    return (
-        <Animated.View
-            style={[
-                styles.card,
-                animatedCard,
-                { 
-                    zIndex: numOfCards - currIndex,
-                    // opacity: 0.4,
-                    // opacity: 1 - currIndex * 0.1, // removing opacioty as we are using animated card styles
-                    transform: [
-                        // {translateY: -currIndex * 36},
-                        // {scale: 1 - currIndex * .1}, // each subseques cards will be 10% smaller than its previous card
-                        // {translateY: -currIndex * 60}
-                    ] 
-                }
-            ]}
-        >
-            <Image style={[StyleSheet.absoluteFillObject, styles.image]} source={{ uri: user.image }} />
-            <LinearGradient
-                // Background Linear Gradient
-                colors={['transparent', 'rgba(0,0,0,0.8)']}
-                style={[StyleSheet.absoluteFillObject, styles.background]}
-            />
-            <View style={styles.footer}>
-                <Text style={styles.name}>{user.name}</Text>
-            </View>
-        </Animated.View>
-    )
-}
+      activeIndex.value = interpolate(
+        Math.abs(translationX.value),
+        [0, 500],
+        [index, index + 0.8]
+      );
+    })
+    .onEnd((event) => {
+      if (Math.abs(event.velocityX) > 400) {
+        translationX.value = withSpring(Math.sign(event.velocityX) * 500, {
+          velocity: event.velocityX,
+        });
+        activeIndex.value = withSpring(index + 1);
+
+        runOnJS(onResponse)(event.velocityX > 0);
+      } else {
+        translationX.value = withSpring(0);
+      }
+    });
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <Animated.View
+        style={[
+          styles.card,
+          animatedCard,
+          {
+            zIndex: numOfCards - index,
+          },
+        ]}
+      >
+        <Image
+          style={[StyleSheet.absoluteFillObject, styles.image]}
+          source={{ uri: user.image }}
+        />
+
+        <LinearGradient
+          // Background Linear Gradient
+          colors={['transparent', 'rgba(0,0,0,0.8)']}
+          style={[StyleSheet.absoluteFillObject, styles.overlay]}
+        />
+
+        <View style={styles.footer}>
+          <Text style={styles.name}>{user.name}</Text>
+        </View>
+      </Animated.View>
+    </GestureDetector>
+  );
+};
 
 const styles = StyleSheet.create({
-    // card: {
-    //     width: TinderCardWidth,
-    //     // height: TinderCardWidth * 1.67  // golden ration
-    //     aspectRatio: 1 / 1.67, // golden ration
+  card: {
+    width: tinderCardWidth,
+    // height: tinderCardWidth * 1.67,
+    aspectRatio: 1 / 1.67,
+    borderRadius: 15,
+    justifyContent: 'flex-end',
 
-    //     alignItems: "flex-end",
-    //     // alignItems: "center",
-    //     // marginTop: "auto",
+    position: 'absolute',
 
-    //     position: "absolute",
-
-    //     borderRadius: 15,
-    //     overflow: "hidden",
-
-    //     shadowColor: "#000",
-    //     shadowOffset: {
-    //         width: 0,
-    //         height: 1
-    //     },
-    //     shadowOpacity: 0.22,
-    //     shadowRadius: 2.22,
-
-    //     elevation: 3
-    // },
-
-    card: {
-        width: TinderCardWidth,
-        // height: tinderCardWidth * 1.67,
-        aspectRatio: 1 / 1.67,
-        borderRadius: 15,
-        justifyContent: 'flex-end',
-
-        position: 'absolute',
-
-        // shadow
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.22,
-        shadowRadius: 2.22,
-
-        elevation: 3,
+    // shadow
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    image: {
-        borderRadius: 15
-    },
-    name: {
-        fontSize: 24,
-        color: "red",
-        fontFamily: "InterBold"
-    },
-    footer: {
-        padding: 10
-    },
-    background: {
-        top: "50%",
-        borderBottomLeftRadius: 15,
-        borderBottomRightRadius: 15
-    }
-})
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
 
-/**
- * 
- * 
- export default function TinderCard() {
-    return (
-        <View style={styles.card}>
-            <Image style={[StyleSheet.absoluteFillObject, styles.image]} source={{ uri: "https://picsum.photos/id/64/200/300" }} />
-            <LinearGradient
-                // Background Linear Gradient
-                colors={['transparent', 'rgba(0,0,0,0.8)']}
-                style={[StyleSheet.absoluteFillObject, styles.background]}
-            />
-            <View style={styles.footer}>
-                <Text style={styles.name}>Dani</Text>
-            </View>
-        </View>
-    )
-}
- */
+    elevation: 3,
+  },
+  image: {
+    borderRadius: 15,
+  },
+  overlay: {
+    top: '50%',
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+  },
+  footer: {
+    padding: 10,
+  },
+  name: {
+    fontSize: 24,
+    color: 'white',
+    fontFamily: 'InterBold',
+  },
+});
+
+export default TinderCard;
