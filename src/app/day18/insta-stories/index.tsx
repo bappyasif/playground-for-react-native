6cc1a9572bfb189f875cf33200c66ea95d0eda4d
bappyasif <bappyasif@hotmail.com>
2024-07-18 21:06:56 +0600

    added animation and modularity
    * igstories related to each user is now moved to a component
    * adding a 3d cube transformation to rotate for each user stories

--- src/app/day18/insta-stories/index.tsx
+++ src/app/day18/insta-stories/index.tsx
@@ -1,160 +1,82 @@
+import IgStories from '@/components/day18/IgStories';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   StyleSheet,
-  Text,
-  View,
-  Image,
   SafeAreaView,
-  TextInput,
-  Pressable,
+  View,
+  Button,
 } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  Easing,
-  useAnimatedReaction,
-  runOnJS,
-} from 'react-native-reanimated';
+const pages = ["#eqf3fa", "#388d46"]
 
-import usersStories from '../../../../assets/data/day18/stories';
-
-const storyViewDuration = 5 * 1000;
-
-export default function IgStories() {
-  const [userIndex, setUserIndex] = useState(0);
-  const [storyIndex, setStoryIndex] = useState(0);
-
-  const progress = useSharedValue(0); // 0 -> 1
-
-  const user = usersStories[userIndex];
-  const story = user.stories[storyIndex];
-
-  useEffect(() => {
+export default function InstaStories() {
+  const width = 200;
+  const progress = useSharedValue(0);
+  const pageIndex = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        perspective: 301
+      },
+      {
+        rotateY: `${interpolate(progress.value, [0, 1], [-90, 90])}deg`
+        // rotateY: "-70deg"
+        // rotateZ: "45deg"
+      }
+    ]
+  }))
+  const runAnimation = () => {
     progress.value = 0;
-    progress.value = withTiming(1, {
-      duration: storyViewDuration,
-      easing: Easing.linear,
-    });
-  }, [storyIndex, userIndex]);
-
-  const goToPrevStory = () => {
-    setStoryIndex((index) => {
-      if (index === 0) {
-        goToPrevUser();
-        return 0;
-      }
-      return index - 1;
-    });
-  };
-
-  const goToNextStory = () => {
-    setStoryIndex((index) => {
-      if (index === user.stories.length - 1) {
-        goToNextUser();
-        return 0;
-      }
-      return index + 1;
-    });
-  };
-
-  const goToNextUser = () => {
-    setUserIndex((index) => {
-      if (index === usersStories.length - 1) {
-        return 0;
-      }
-      return index + 1;
-    });
-  };
-
-  const goToPrevUser = () => {
-    setUserIndex((index) => {
-      if (index === 0) {
-        return usersStories.length - 1;
-      }
-      return index - 1;
-    });
-  };
-
-  useAnimatedReaction(
-    () => progress.value,
-    (currentValue, previousValue) => {
-      if (currentValue !== previousValue && currentValue === 1) {
-        runOnJS(goToNextStory)();
-      }
-    }
-  );
-
-  const indicatorAnimatedStyle = useAnimatedStyle(() => ({
-    width: `${progress.value * 100}%`,
-  }));
-
+    // progress.value = withTiming(1);
+    progress.value = withTiming(1, { duration: 2999 });
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.storyContainer}>
-        <Image source={{ uri: story.uri }} style={styles.image} />
+      {/* <IgStories /> */}
+      <StatusBar style='light' />
+      {/* <IgStories /> */}
 
-        <Pressable style={styles.navPressable} onPress={goToPrevStory} />
-
-        <Pressable
-          style={[styles.navPressable, { right: 0 }]}
-          onPress={goToNextStory}
-        />
-
-        <View style={styles.header}>
-          <LinearGradient
-            // Background Linear Gradient
-            colors={['rgba(0,0,0,0.7)', 'transparent']}
-            style={StyleSheet.absoluteFill}
+      {
+        pages.map((pgClr) => (
+          <Animated.View
+            key={pgClr}
+            style={[{
+              width, position: "absolute",
+              aspectRatio: 9 / 16, backgroundColor: pgClr, borderRadius: 10,
+              // backfaceVisibility: "hidden",
+              transformOrigin: ["50%", "50%", -width / 2],
+            }, animatedStyle]}
           />
-          <View style={styles.indicatorRow}>
-            {user.stories.map((story, index) => (
-              <View key={`${user.userId}-${index}`} style={styles.indicatorBG}>
-                {/* <View
-                  style={[
-                    styles.indicator,
-                    {
-                        backgroundColor: index <= storyIndex ? "floralwhite" : "gray"
-                    }
-                  ]}
-                /> */}
-                {/* <Animated.View
-                  style={[
-                    styles.indicator,
-                    indicatorAnimatedStyle
-                  ]}
-                /> */}
-                {/* <Animated.View
-                  style={[
-                    styles.indicator,
-                    index === storyIndex && indicatorAnimatedStyle,
-                  ]}
-                /> */}
-                <Animated.View
-                  style={[
-                    styles.indicator,
-                    index === storyIndex && indicatorAnimatedStyle,
-                    index > storyIndex && { width: 0 },
-                    index < storyIndex && { width: '100%' },
-                  ]}
-                />
-              </View>
-            ))}
-          </View>
+        ))
+      }
 
-          <Text style={styles.username}>{user.username}</Text>
-        </View>
-      </View>
-      <View style={styles.footer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Send message"
-          placeholderTextColor="white"
-        />
-      </View>
+      {/* <Animated.View 
+        style={[{
+          width, aspectRatio: 9/16, backgroundColor: "#e1f3fa", borderRadius: 10,
+          // backfaceVisibility: "hidden",
+          transformOrigin: ["50%", "50%", -width / 2],
+        }, animatedStyle]} 
+        /> */}
+      <Button title='Run Animation' onPress={runAnimation} />
+
+      {/* <View 
+        style={{
+          width, aspectRatio: 9/16, backgroundColor: "#e1f3fa", borderRadius: 10,
+          // transformOrigin: ["center", "center", width / 2],
+          transformOrigin: ["50%", "50%", -width / 2],
+          transform: [
+            {
+              perspective: 301
+            },
+            {
+              rotateY: "-70deg"
+              // rotateZ: "45deg"
+            }
+          ]
+        }} 
+        /> */}
     </SafeAreaView>
   );
 }
@@ -163,60 +85,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-  },
-  storyContainer: {
-    flex: 1,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-  },
-  header: {
-    position: 'absolute',
-    top: 0,
-    // backgroundColor: 'rgba(0, 0, 0, 0.25)',
-    width: '100%',
-    padding: 20,
-    paddingTop: 10,
-  },
-  username: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  footer: {
-    width: '100%',
-    backgroundColor: 'black',
-    padding: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 15,
-    borderRadius: 50,
-    color: 'white',
-  },
-  navPressable: {
-    position: 'absolute',
-    width: '30%',
-    height: '100%',
-  },
-
-  indicatorRow: {
-    gap: 5,
-    flexDirection: 'row',
-    marginBottom: 20,
-  },
-
-  indicatorBG: {
-    flex: 1,
-    height: 3,
-    backgroundColor: 'gray',
-    borderRadius: 10,
-    overflow: 'hidden',
-  },
-  indicator: {
-    backgroundColor: 'white',
-    height: '100%',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
